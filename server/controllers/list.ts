5dca2cd8a700d76245f49be5400bc8616478aaf9
Hazrat Ali <iloveyii@yahoo.com>
2020-06-07 00:57:09 +0200

    Initial server

---
+++ server/controllers/list.ts
@@ -0,0 +1,24 @@
+
+// @desc   Get all from List
+// @route  GET /api/v1/list
+export const getList = async (req: any, res: any, next: any) => {
+
+};
+
+// @desc   Add to List
+// @route  POST /api/v1/list
+export const addList = async (req: any, res: any, next: any) => {
+
+};
+
+// @desc   Update List
+// @route  PUT /api/v1/list
+export const updateList = async (req: any, res: any, next: any) => {
+
+};
+
+// @desc   Delete List
+// @route  DELETE /api/v1/list
+export const deleteList = async (req: any, res: any, next: any) => {
+
+};
---
+++ server/server.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import cors from "cors";
+import compression from "compression";
+import dotenv from "dotenv";
+
+
+// ----------------------------------
+// Routes Import
+// ----------------------------------
+import list from "./routes/list";
+
+// ----------------------------------
+// Environment setup
+// ----------------------------------
+dotenv.config({path: "./configs/.env"});
+
+// ----------------------------------
+// Connect to DB
+// ----------------------------------
+// const connectDB = require("./config/db");
+// connectDB();
+
+// ----------------------------------
+// Express configuration
+// ----------------------------------
+const app: any = express();
+app.use(express.json());
+app.use(cors());
+app.use(compression());
+app.use(express.urlencoded({extended: true}));
+
+// ----------------------------------
+// API Routes
+// ----------------------------------
+app.use("/api/v1/list", list);
+
+// ----------------------------------
+// Express server
+// ----------------------------------
+const PORT = process.env.PORT || 5000;
+try {
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+} catch (e) {
+    console.log(`Cannot start server on port ${PORT}`);
+}
+
